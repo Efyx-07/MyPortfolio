24676aeca3653b86f3982e03e57ee8c812aa8f82
François-Xavier Savary <fx.savary@orange.fr>
2024-05-27 17:10:44 +0200

    HeroSection

--- src/app/components/HeroSection/HeroText.tsx
+++ src/app/components/HeroSection/HeroText.tsx
@@ -1,7 +1,24 @@
+import { Icon } from '@iconify/react';
+import './HeroText.scss';
+
 export default function HeroText() {
     return (
-        <div>
-            HeroText
+        <div className="hero-text">
+            <div className="text-container">
+                <div className="textIcon-container hello">
+                    <Icon icon="solar:hand-shake-outline" className="icon"/>
+                    <p>Hello ! Je suis FX</p>
+                </div>
+                <h1>Développeur <span>Frontend</span></h1>
+                <div className="textIcon-container">
+                    <Icon icon="ic:baseline-place" className="icon"/>
+                    <p>Lille, France</p>
+                </div>
+            </div>
+            <div className="buttons-container">
+                <button>Hello</button>
+                <button>Goodbye</button>
+            </div>
         </div>
     )
 }
