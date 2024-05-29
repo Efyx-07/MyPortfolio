da2d28e26b59253f316ba3bf45c9e7cdb18b6bb1
François-Xavier Savary <fx.savary@orange.fr>
2024-05-29 17:15:58 +0200

    update architecture

--- src/app/hooks/useContacts.ts
+++ src/app/hooks/useContacts.ts
@@ -19,39 +19,4 @@ export default function useContacts() {
     }, []);
 
     return contacts;
-}
-
-/*
-export default function useContacts() {
-    const [contacts, setContacts] = useState<Contact[]>([]);
-
-    const fetchDataFromApi = async () => {
-        try {
-            const response = await fetch('/api/contacts', {
-                headers: {
-                    Accept: "application/json",
-                    method: "GET",
-                },
-            });
-            if (response.ok) {
-                const data = await response.json();
-                if (data.contacts) {
-                    setContacts(data.contacts);
-                } else {
-                    console.error('Not the expected format', data);
-                }
-            } else {
-                console.error('Error while fetching datas', response.status);
-            }
-        } catch (error) {
-            console.error('Error while fetching datas', error);
-        }
-    };
-
-    useEffect(() => {
-        fetchDataFromApi();
-    }, []);
-
-    return contacts;
-}
-*/
+};
