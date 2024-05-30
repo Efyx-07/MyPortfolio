492f40f7a6b96a9cfff3c9727f9fbab681dfde37
Efyx-07 <fx.savary@orange.fr>
2024-05-31 00:08:44 +0200

    route

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -5,26 +5,7 @@ import Project from "@/app/types/projectType";
 const projects: Project[] = projectsData.projects;
 
 export async function GET() {
-
-    const isSSR = process.env.NEXT_PUBLIC_IS_SSR === 'true';
-
-    if (isSSR) {
-        return NextResponse.json({
-            projects   
-        },
-        {
-            headers: {
-                'Cache-Control': 'no-store'
-            },
-        })       
-    } else {
-        return NextResponse.json({
-            projects   
-        },
-        {
-            headers: {
-                'Cache-Control': 'public, max-age=3600'
-            },
-        })
-    } 
-}
+    return NextResponse.json({
+        projects   
+    });
+};
