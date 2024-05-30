ab086a26df4277ebb3f8e127b644f82d90306919
Efyx-07 <fx.savary@orange.fr>
2024-05-31 01:42:54 +0200

    cache issue fixed

--- src/app/services/fetchProjects.ts
+++ src/app/services/fetchProjects.ts
@@ -1,10 +1,14 @@
 import Project from '../types/projectType';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchProjects(): Promise<Project[]> {
+
+    noStore(); // allow the data dynamic render for SSR page
+    
     const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/projects`, {
         headers: {
             Accept: "application/json",
-            method: "GET",
+            method: "GET"
         },
     });
 
