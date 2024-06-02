4181b2ffd72880700f8a5a3c1f8de0294b3530a8
Efyx-07 <fx.savary@orange.fr>
2024-06-02 03:06:43 +0200

    appinit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
-import { useState, useEffect } from 'react';
-import { useContacts, useProjects, useSkills } from './hooks';
-import { Contact, Project, Skill } from './types';
+
+import useAppInit from './hooks/useAppInit';
 import LoadingPage from './components/LoadingPage';
 import HeroSection from './components/HeroSection';
 import ProjectSection from './components/ProjectSection';
@@ -11,33 +10,7 @@ import '../assets/sass/pages-common-style.scss';
 
 export default function Home() {
 
-  const [contactsLoaded, setContactsLoaded] = useState<boolean>(false);
-  const [projectsLoaded, setProjectsLoaded] = useState<boolean>(false);
-  const [skillsLoaded, setSkillsLoaded] = useState<boolean>(false);
-
-  const contacts: Contact[] = useContacts();
-  const projects: Project[] = useProjects();
-  const skills: Skill[] = useSkills();
-
-  useEffect(() => {
-    if (contacts.length > 0) {
-      setContactsLoaded(true);
-    }
-  }, [contacts]);
-
-  useEffect(() => {
-    if (projects.length > 0) {
-      setProjectsLoaded(true);
-    }
-  }, [projects]);
-
-  useEffect(() => {
-    if (skills.length > 0) {
-      setSkillsLoaded(true);
-    }
-  }, [skills]);
-
-  const allDataLoaded = contactsLoaded && projectsLoaded && skillsLoaded;
+  const allDataLoaded: boolean = useAppInit();
 
   return (
     <div className='page'>
