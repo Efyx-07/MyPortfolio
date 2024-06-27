b1d61b9cdcd1f5eaf7af52453883a7c30b2ddb12
François-Xavier Savary <fx.savary@orange.fr>
2024-06-27 16:30:09 +0200

    UI header hero-section

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
 
     return (
         <footer>
-            <p>© {year} | développé par FX en Next.js </p>
+            <p>© {year} | développement et design par FX</p>
         </footer>
     )
 }
