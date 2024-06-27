178a02b7c3abc1af690073fa18cc032f8f91dc74
François-Xavier Savary <fx.savary@orange.fr>
2024-06-27 14:22:03 +0200

    UI contact section

--- src/app/components/ContactSection/index.tsx
+++ src/app/components/ContactSection/index.tsx
@@ -11,25 +11,23 @@ export default function ContactSection() {
 
     return (
         <section className="contact-section">
-            <div className="contact-content">
-                <div className="contact-block">
-                    <div className="contact-text">
-                        <h2>Envie de travailler avec moi ?</h2>
-                        <p>N&apos;hésitez pas à me contacter par mail ou via mes réseaux sociaux.</p>
-                    </div>
-                    <div className="buttons-container">
-                        {contacts.map((contact, index) => (
-                            <Button 
-                                key={contact.name}
-                                name={contact.name}
-                                icon={contact.icon}
-                                link={contact.link}
-                                target={contact.target}
-                                rel={contact.rel}
-                                className={index === 2 ? 'contact-button email-button' : 'contact-button'}
-                            />  
-                        ))}
-                    </div>
+            <div className="content contact-content">
+                <div className="contact-text">
+                    <h1>Envie de travailler <span>avec moi ?</span></h1>
+                    <p>N&apos;hésitez pas à me contacter par mail ou via mes réseaux sociaux.</p>
+                </div>
+                <div className="contact-buttons-container">
+                    {contacts.map((contact, index) => (
+                        <Button 
+                            key={contact.name}
+                            name={contact.name}
+                            icon={contact.icon}
+                            link={contact.link}
+                            target={contact.target}
+                            rel={contact.rel}
+                            className={index === 2 ? 'contact-button email-button' : 'contact-button'}
+                        />  
+                    ))}
                 </div>
             </div>
         </section>
