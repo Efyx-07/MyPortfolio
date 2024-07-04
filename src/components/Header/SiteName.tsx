7a642ad13f2cb8d7b1f6696f014df9a20242f15a
François-Xavier Savary <fx.savary@orange.fr>
2024-07-04 16:16:20 +0200

    header update

--- src/components/Header/SiteName.tsx
+++ src/components/Header/SiteName.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import './SiteName.scss';
 
 export default function SiteName() {
@@ -6,6 +7,21 @@ export default function SiteName() {
     const router = useRouter();
 
     return(
-        <p className="site-name" onClick={() => router.push('/')}><span>FX</span>Savary.dev</p>
+        <div 
+            className="siteName-logo-container"
+            onClick={() => router.push('/')}
+        >
+            <div className="logo-container">
+                <Image 
+                    className="logo"
+                    src='/images/decoration/logo.png'
+                    width={500}
+                    height={500}
+                    alt="fxsavary.com" 
+                    priority
+                />
+            </div>
+            <p className="site-name"><span>FX</span>Savary.dev</p>
+        </div>
     )
 }
