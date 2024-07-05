49d00f323e43a448e69a4cf8fab169e9e444699c
François-Xavier Savary <fx.savary@orange.fr>
2024-07-05 12:08:40 +0200

    SEO 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,10 @@ export const metadata: Metadata = {
     title: "François-Xavier Savary | Développeur frontend",
     description: "Hello, je suis François-Xavier, développeur frontend basé à Lille en France. Passionné par l’UI et l’expérience utilisateur, je développe des applications et sites web réactifs au design soigné. Ma stack principale: TypeScript, React, NextJs, VueJs, Sass, NodeJs",
     url: 'https://www.fxsavary.com/',
+    type: 'website',
     images: [
       {
-        url: 'https://www.fxsavary.com/images/screenshots/fxsavary_portfolio_thumbnail.png',
+        url: 'https://www.fxsavary.com/images/decoration/fxsavary_portfolio_thumbnail.png',
         width: 800,
         height: 600,
         alt: 'François-Xavier Savary | Développeur frontend',
