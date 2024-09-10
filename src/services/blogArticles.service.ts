22c6022b99ea51690e0104fe9978ac39db37d7d3
François-Xavier Savary <fx.savary@orange.fr>
2024-09-10 11:33:17 +0200

    blogarticles service

--- src/services/blogArticles.service.ts
+++ src/services/blogArticles.service.ts
@@ -17,3 +17,19 @@ export async function fetchArticles(): Promise<Article[]> {
     throw new Error('Error while fetching articles: ' + error);
   }
 }
+
+// Récupère un article par son ID, retourne l'article en réponse
+// ===========================================================================================
+export async function fetchArticleById(id: Article['id']): Promise<Article> {
+  try {
+    const response = await fetch(`${backendUrl}/articles/${id}`);
+    if (!response.ok) {
+      throw new Error(`Error while fetching article ${id}`);
+    }
+    const data: { article: Article } = await response.json();
+    const article: Article = data.article;
+    return article;
+  } catch (error) {
+    throw new Error('Error while fetching article: ' + error);
+  }
+}
