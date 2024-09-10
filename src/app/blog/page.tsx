ac81031a9f382616888907892429d764d59bea50
François-Xavier Savary <fx.savary@orange.fr>
2024-09-10 15:39:02 +0200

    blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { cardVariants } from '@/framer-motion/cardVariants';
+import { Article } from '@/types/article.interface';
+import ArticleCard from '@/components/ArticleSection/ArticleCard';
+import { useQuery } from '@tanstack/react-query';
+import { fetchArticles } from '@/services';
+import '../../assets/sass/pages-common-style.scss';
+import '../../assets/sass/sections-common-style.scss';
+import './blog-page.scss';
+
+export default function BlogPage() {
+  const { data: articles = [] } = useQuery<Article[]>({
+    queryKey: ['articles'],
+    queryFn: fetchArticles,
+  });
+
+  return (
+    <div className="page">
+      <div className="content">
+        <section>
+          <div className="content">
+            <div className="article-cards-container">
+              {articles.map((article, index) => (
+                <motion.div
+                  key={article.id}
+                  custom={index}
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true }}
+                  variants={cardVariants}
+                  className="article-card"
+                >
+                  <ArticleCard key={article.id} article={article} />
+                </motion.div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
