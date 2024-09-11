d056de63ecb6ead977bae220183dff450356e547
François-Xavier Savary <fx.savary@orange.fr>
2024-09-11 13:21:51 +0200

    menuIcon sass

--- src/components/Header/MobileMenuIcon.tsx
+++ src/components/Header/MobileMenuIcon.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '@iconify/react';
 import './MobileMenuIcon.scss';
 
 interface MobileMenuIconProps {
@@ -11,7 +12,11 @@ export default function MobileMenuIcon({
 }: MobileMenuIconProps) {
   return (
     <div className="mobile-menu-icon" onClick={toggleMenu}>
-      {isOpen ? <h1>X</h1> : <h1>O</h1>}
+      {isOpen ? (
+        <Icon icon="line-md:menu-to-close-transition" />
+      ) : (
+        <Icon icon="material-symbols:menu" />
+      )}
     </div>
   );
 }
