1b940c109dcfeb6d361ff6debdfce8256d8dbd48
François-Xavier Savary <fx.savary@orange.fr>
2024-09-11 10:27:36 +0200

    burger menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google';
 import Providers from './providers';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import BurgerMenu from '@/components/BurgerMenu';
 import '../assets/sass/global.scss';
 
 export const metadata: Metadata = {
@@ -47,6 +48,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Footer />
+          <BurgerMenu />
         </Providers>
       </body>
     </html>
---
+++ src/components/BurgerMenu/index.tsx
@@ -0,0 +1,9 @@
+import './BurgerMenu.scss';
+
+export default function BurgerMenu() {
+  return (
+    <div className="burger-menu-modal">
+      <h1>BURGER MENU</h1>
+    </div>
+  );
+}
