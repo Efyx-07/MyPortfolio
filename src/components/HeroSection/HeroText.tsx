eba7a32b9bc808f5efb18be679ab1fc030c28055
François-Xavier Savary <fx.savary@orange.fr>
2025-06-10 15:17:32 +0200

    hero update

--- src/components/HeroSection/HeroText.tsx
+++ src/components/HeroSection/HeroText.tsx
@@ -41,7 +41,12 @@ export default function HeroText() {
         >
           <p className="presentation-text">
             Passionné par l’UI et l’expérience utilisateur, je développe des
-            applications et sites web réactifs au design soigné.
+            applications et sites web réactifs au design soigné. Je peux aussi
+            intervenir sur des projets fullstack.
+            <br></br>
+            <span className="presentation-added-mention">
+              Actuellement en CDI chez Groupe NAT.
+            </span>
           </p>
         </motion.div>
         <Separator />
